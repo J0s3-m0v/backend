ff97efcb186631d9598ec3ccc809dc74f0b9624d
J0s3-m0v <j0s3.m0v@outlook.es>
2020-08-13 16:40:57 +0100

    develope api job backend

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port =normalizePort(process.env.Port)|| 3700;
+
+// here made the connection with the data base
+
+mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://localhost:27017/jobDB')
+mongoose.connect('mongodb+srv://admin:admin@cluster0.vm8xd.mongodb.net/jobDB?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("The Database was connected successfully");
+    // create server
+    app.listen(port,()=>{
+        console.log("Server running correctly at the url:localhost:3700");
+    });
+})
+.catch(err =>console.log(err));
